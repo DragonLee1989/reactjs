a840920858e5f3f7357a2a8f1ff76f6afe52869a
DragonLee1989 <dragonleeaht2019@gmail.com>
2023-07-07 11:30:11 +0700

    add api creatUser

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,33 +1,80 @@
-import React, { Component } from 'react';
-import { FormattedMessage } from 'react-intl';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import "./UserManage.scss";
+import { getAllUsers } from "../../services/userService";
 class UserManage extends Component {
+  //   state = {};
+  // tao constructor => init state
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrayUsers: [],
+    };
+  }
 
-    state = {
-
+  // ham goi API => DidMount => set state: ham gan gia tri cho cac bien state
+  async componentDidMount() {
+    let response = await getAllUsers("ALL");
+    if (response && response.errCode == 0) {
+      this.setState({
+        arrayUsers: response.users,
+      });
     }
+    console.log("getAllUsers: ", this.state.arrayUsers);
+  }
 
-    componentDidMount() {
-
-    }
-
-
-    render() {
-        return (
-            <div className="text-center">Manage users</div>
-        );
-    }
-
+  render() {
+    console.log("CHECK STATE: ", this.state);
+    let arrUsers = this.state.arrayUsers;
+    return (
+      <div className="users-container">
+        <div className="title text-center">Manage Users</div>
+        <div className="user-table mt-3 mx-1">
+          <table id="customers">
+            <tr>
+              <th className="col-1 text-center">No</th>
+              <th className="col-4">Email</th>
+              <th className="col-1">First Name</th>
+              <th className="col-1">Last Name</th>
+              <th className="col-4">Address</th>
+              <th className="col-1 text-center">Actions</th>
+            </tr>
+            {/* ham "map" thay cho vong lap "for" */}
+            {arrUsers &&
+              arrUsers.map((item, index) => {
+                console.log("CHECK MAP: ", item, index);
+                return (
+                  <tr key={index}>
+                    <td className="text-center">{index}</td>
+                    <td>{item.email}</td>
+                    <td>{item.firstName}</td>
+                    <td>{item.lastName}</td>
+                    <td>{item.address}</td>
+                    <td>
+                      <button className="btn-edit">
+                        <i class="fas fa-edit"></i>
+                      </button>
+                      <button className="btn-del">
+                        <i class="fas fa-trash"></i>
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-    };
+const mapStateToProps = (state) => {
+  return {};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
--- src/services/userService.js
+++ src/services/userService.js
@@ -4,4 +4,9 @@ const handleLoginApi = (emailUser, passwordUser) => {
   return axios.post("/api/login", { email: emailUser, password: passwordUser });
 };
 
-export { handleLoginApi };
+const getAllUsers = (userId) => {
+  // template string `/api/get-all-users?id=${userId}`
+  return axios.get(`/api/get-all-users?id=${userId}`);
+};
+
+export { handleLoginApi, getAllUsers };
